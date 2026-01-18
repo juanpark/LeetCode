83acc5ebce8bd7a74383410280af3a0a12e4df92
Jeong Hwan Park <juanpark@me.com>
2026-01-18 22:15:29 +0900

    [Bronze II] Title: 9진수, Time: 100 ms, Memory: 14140 KB -BaekjoonHub

--- 백준/Bronze/14491. 9진수/9진수.java
+++ 백준/Bronze/14491. 9진수/9진수.java
@@ -1,19 +1,9 @@
 import java.io.*;
 
 class Main {
-
-    static final int RADIX = 9;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
-
-        StringBuilder sb = new StringBuilder();
-        while (T > 0) {
-            sb.append(T % RADIX);
-            T /= RADIX;
-        }
-
-        System.out.print(sb.reverse());
+        System.out.print(Integer.toString(T, 9));
     }
 }
