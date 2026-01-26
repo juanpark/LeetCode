c9bb4e0af22400f176d63aff8e808ea4e29a452c
Jeong Hwan Park <juanpark@me.com>
2026-01-26 10:41:48 +0900

    [Bronze II] Title: 유학 금지, Time: 100 ms, Memory: 14152 KB -BaekjoonHub

--- 백준/Bronze/2789. 유학 금지/유학 금지.java
+++ 백준/Bronze/2789. 유학 금지/유학 금지.java
@@ -5,12 +5,15 @@ class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         char[] word = br.readLine().toCharArray();
-        Set<Character> compSet = new HashSet<>(Arrays.asList('C','A','M','B','R','I','D','G','E'));
+        boolean[] ban = new boolean[26];
+        for (char c : "CAMBRIDGE".toCharArray()) {
+            ban[c - 'A'] = true;   
+        }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < word.length; i++) {
-            if (compSet.contains(word[i])) continue;
-            sb.append(word[i]);
+        for (char c : word) {
+            if (ban[c - 'A']) continue;
+            sb.append(c);
         }
 
         System.out.print(sb);
