647c6f4df825c9d0088e051f5cfbf508f4952fbb
Jeong Hwan Park <juanpark@me.com>
2026-01-29 14:44:46 +0900

    [Bronze IV] Title: 입실 관리, Time: 100 ms, Memory: 14236 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5524. 입실 관리/입실 관리.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        while (N-- > 0) {
+            sb.append(br.readLine().toLowerCase()).append('\n');
+        }
+        System.out.print(sb);
+    }
+}
